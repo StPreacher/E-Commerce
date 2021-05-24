0a4bf4d221b1ffdc9128372459e0c3a32d9ff075
Mucahid <mucahidd3@gmail.com>
2021-05-24 16:53:06 +0300

    Firebase Integrated

--- app/src/main/java/com/mdgn/ecommerce/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/mdgn/ecommerce/ui/category/CategoryFragment.kt
@@ -10,7 +10,6 @@ import com.mdgn.ecommerce.R
 
 class CategoryFragment : Fragment() {
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
