823c9d7eb8b79f11d45e84ead09d3092ccbcb6ee
Mucahid <mucahidd3@gmail.com>
2021-06-15 23:39:16 +0300

    Try to cart fragment

--- app/src/main/java/com/mdgn/ecommerce/ui/cart/CartFragment.kt
+++ app/src/main/java/com/mdgn/ecommerce/ui/cart/CartFragment.kt
@@ -1,21 +1,53 @@
 package com.mdgn.ecommerce.ui.cart
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mdgn.ecommerce.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.mdgn.ecommerce.databinding.FragmentCartBinding
 
 
 class CartFragment : Fragment() {
 
+    private lateinit var binding : FragmentCartBinding
+    private lateinit var mDatabase : FirebaseFirestore
+    private lateinit var mAuth : FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+    ): View {
+        binding = FragmentCartBinding.inflate(layoutInflater,container,false)
+        val view = binding.root
+        mDatabase = FirebaseFirestore.getInstance()
+        mAuth = FirebaseAuth.getInstance()
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val docRef = mDatabase
+            .collection("users")
+            .document(mAuth.currentUser?.uid.toString())
+            .collection("cart")
+
+        docRef.get().addOnSuccessListener {
+            if (it != null){
+                Log.d("GetCart", "Products of cart : ${it.documents}")
+            }else{
+                Log.d("GetCart", "No such document")
+
+            }
+        }.addOnFailureListener {
+            Log.d("GetCart", "get failed with ", it)
+
+        }
+
     }
 
 
