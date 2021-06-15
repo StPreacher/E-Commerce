74ee05d0521a83ac8ab02b7fdeaae4e4617f8074
Mucahid <mucahidd3@gmail.com>
2021-06-15 22:10:47 +0300

    Write cart datas to firestore

--- app/src/main/java/com/mdgn/ecommerce/ui/category/adapter/ProductAdapter.kt
+++ app/src/main/java/com/mdgn/ecommerce/ui/category/adapter/ProductAdapter.kt
@@ -1,9 +1,8 @@
 package com.mdgn.ecommerce.ui.category.adapter
 
-import android.content.Context
+
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -13,13 +12,10 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.mdgn.ecommerce.R
 import com.mdgn.ecommerce.databinding.ProductDetailBinding
 import com.mdgn.ecommerce.model.ProductDetailList
-import com.mdgn.ecommerce.ui.category.AddToCartClickListener
 import com.mdgn.ecommerce.util.getProgressDrawable
 import com.mdgn.ecommerce.util.loadImage
-import kotlinx.android.synthetic.main.product_detail.view.*
 
-class ProductAdapter (var productDetailList: List<ProductDetailList>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>()
-,AddToCartClickListener{
+class ProductAdapter (var productDetailList: List<ProductDetailList>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
     private val mUser = FirebaseAuth.getInstance().currentUser
     private val mDatabase = FirebaseFirestore.getInstance()
@@ -46,7 +42,6 @@ class ProductAdapter (var productDetailList: List<ProductDetailList>) : Recycler
             .product_image_url, getProgressDrawable(
             holder.view.productImage.context
             ))
-        holder.view.listener = this
         holder.view.addToCartButton.setOnClickListener {
             val addedProducts = hashMapOf(
                 "imageUrl" to productDetailList[position].product_image_url,
@@ -68,27 +63,6 @@ class ProductAdapter (var productDetailList: List<ProductDetailList>) : Recycler
 
     override fun getItemCount(): Int = productDetailList.size
 
-    override fun onAddToCartClicked(v: View) {
-
-        v.addToCartButton.setOnClickListener {
-            val addedProducts = hashMapOf(
-            "imageUrl" to v.productImage?.toString(),
-            "productName" to v.productName?.text.toString(),
-            "productDetail" to v.productDetails?.text.toString(),
-            "productPrice" to v.productPrice?.text.toString(),
-            "productID" to v.productID?.text.toString()
-        )
-            mDatabase.collection("users")
-                .document(mUser?.uid.toString())
-                .collection("cart")
-                .document(v.productID.text.toString())
-                .set(addedProducts)
-                .addOnSuccessListener {
-                    Log.d("AddCart", "DocumentSnapshot successfully written!") }
-                .addOnFailureListener { e -> Log.w("AddCart", "Error writing document", e) }
-        }
-
-    }
 }
 
 
